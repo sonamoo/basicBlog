ee0401757cb0251db31a3585d8b2753316867c85
Seho Lim <limseho657424@gmail.com>
2016-10-31 16:30:22 -0500

    now the db and the main.html are working.

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import jinja2
 import os
@@ -36,17 +20,38 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 
 
+class Article(db.Model):
+	title = db.StringProperty(required = True)
+	contents = db.StringProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
+	def blog_render(self, title="", contents=""):
+		articles = db.GqlQuery("SELECT * FROM Article "
+							   "ORDER BY created DESC ")
+
+		self.render("main.html", title = title, contents = contents, articles = articles)
+
 	def get(self):
-		self.render("main.html")
-
-
+		self.blog_render()
 
 class NewPost(Handler):
 	def get(self):
 		self.render("newpost.html")
 
+	def post(self):
+		title = self.request.get("title")
+		contents = self.request.get("contents")
+
+		if title and contents:
+			a = Article(title = title, contents = contents)
+			a.put()
+
+			self.redirect("/blog")
+
+		else:
+			error = "We need both a title and the blog content"
+			self.blog_render(title, contents, error)
 
 
 app = webapp2.WSGIApplication([
