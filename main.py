ac293b6b106f76c385b106d48d04b6bf6dd15b8c
Seho Lim <limseho657424@gmail.com>
2016-11-21 18:46:27 -0600

    changed the Handler function (login, logout, initialize, functions for cookies)

--- main.py
+++ main.py
@@ -22,15 +22,51 @@ def render_str(template, **params):
 	t = jinja_env.get_template(template)
 	return t.render(params)
 
+def make_secure_val(val):
+	secured_value = hmac.new(secret, val).hexdigest()
+	return '%s|%s' % (val, secured_value)
+
+def check_secure_val(secure_val):
+	val = secure_val.split('|')[0]
+	if secure_val == make_secure_val(val):
+		return val
+
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
 	def render_str(self, template, **params):
+		params['user'] = self.user
+		t = jinja_env.get_template(template)
 		return render_str(template, **params)
 
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
+		
+	def set_secure_cookie(self, name, val):
+		cookie_val = make_secure_val(val)
+		self.response.headers.add_header(
+			'Set-Cookie',
+			'%s=%s; Path=/' % (name, cookie_val))
+
+	def read_secure_cookie(self, name):
+		cookie_val = self.request.cookies.get(name)
+		return cookie_val and check_secure_val(cookie_val)
+
+	def login(self, user):
+		self.set_secure_cookie('user_id' , str(user.key().id()))
+
+	def logout(self):
+		self.response.headers.add_header(
+			'Set-Cookie',
+			'user_id=; Path=/')
+
+	def initialize(self, *a, **kw):
+		webapp2.RequestHandler.initialize(self, *a, **kw)
+		uid = self.read_secure_cookie('user_id')
+		self.user = uid and User.by_id(int(uid))
+
+
 
 # blog_key is for the data store. It stores
 def blog_key(name = 'default'):
